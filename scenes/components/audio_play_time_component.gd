6afa7360c948552845b20c0d5e7a790489b5dcaf
humensmoc <814103100@qq.com>
2026-05-23 18:48:47 +0800

    - Main menu， Level, AudioBus

---
+++ scenes/components/audio_play_time_component.gd
@@ -0,0 +1,9 @@
+extends Timer
+
+@export var audio_stream_player_2d:AudioStreamPlayer2D
+
+
+
+
+func _on_timeout() -> void:
+	audio_stream_player_2d.play()
--- scenes/objects/chest/chest.gd
+++ scenes/objects/chest/chest.gd
@@ -49,7 +49,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			is_chest_open=true
 			
 			var balloon:BaseGameDialogueBalloon=balloon_scene.instantiate()
-			get_tree().current_scene.add_child(balloon)
+			get_tree().root.add_child(balloon)
 			balloon.start(load("res://dialogue/conversation/chest.dialogue"),dialogue_start_command)
 
 func on_feed_animals()->void:
